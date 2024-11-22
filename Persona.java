514721cc09685a62951a61aaa519c68a57544e9d
Yendi Damaris Galán García <yendi.galan@anahuac.mx>
2024-11-22 11:54:18 -0600

    Update and rename Persona to Persona.java

---
+++ Persona.java
@@ -0,0 +1,14 @@
+public clas Persona {
+  public String name; 
+  public int age;
+  public Persona (){
+    this.name;
+    this.age;
+  }
+   public Persona (){
+    this.name;
+    this.age;
+  }
+  public showInfo{
+    
+}
